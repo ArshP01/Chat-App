1b3baef78c16f27863c72b893e81034a9111ae1f
Arsh <arshphutela01@gmail.com>
2023-03-29 09:14:54 +0530

    Initial Commit

---
+++ Chat App/Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Arsh on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
